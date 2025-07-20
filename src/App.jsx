f68cd9d821ced1ccc15ec1d94df94b19d159035d
prajuyadv <patnamprajukumaryadav@gmail.com>
2025-07-20 09:54:41 -0500

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Testimonials from './components/Testimonials';
+import Contact from './components/contact';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { assets } from '../assets/assets';
+import Navbar from './navbar';
+
+const Header = () => {
+  return (
+    <div
+      className="relative min-h-screen mb-4 bg-cover bg-center w-full"
+      style={{ backgroundImage: `url(${assets.header_img})` }}
+    >
+      <Navbar />
+
+      {/* Fullscreen content area */}
+      <div className="flex items-center justify-center min-h-screen px-6 md:px-20 lg:px-32">
+        <div className="text-center text-white">
+          <h2 className="text-3xl md:text-4xl font-semibold mb-6">
+            Explore homes that fit your dreams
+          </h2>
+
+          <div className="flex justify-center gap-6">
+            <a
+              href="#projects"
+              className="border border-white px-8 py-3 rounded"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="bg-blue-500  px-8 py-3 rounded"
+            >
+              Contact Us
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
