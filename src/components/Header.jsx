95c9461a3d0fd4da46ea03bad4738bb764705afd
prajuyadv <patnamprajukumaryadav@gmail.com>
2025-10-28 15:03:09 -0500

    fixed error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { assets } from '../assets/assets';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 import { motion } from "motion/react"
 const Header = () => {
   return (
